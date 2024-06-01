6ca883d2e9b0fe89453bb6d87b88fce00b35f50c
Abhisht Srivastava <abhisht.srivastava@sap.com>
2024-06-01 15:53:07 +0530

    final changes for xcode provisioning profile

---
+++ InsightforgeX/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,160 @@
+//
+//  LoginView.swift
+//  InsightforgeX
+//
+//  Created by Srivastava, Abhisht on 03/03/24.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseCore
+import GoogleSignIn
+import GoogleSignInSwift
+import AuthenticationServices
+
+struct GoogleSignInResultModel{
+    let idToken : String
+    let accessToken : String
+}
+
+@MainActor
+final class AuthenticationViewModel: ObservableObject{
+    
+    func signInGoogle() async throws{
+    
+        guard let topVC = Utilities.shared.topViewController()
+                
+               else{
+                   throw URLError(.cannotFindHost)
+               }
+        let gidsigninresult = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
+        
+        guard let idToken = gidsigninresult.user.idToken?.tokenString else{
+            throw URLError(.badServerResponse)
+        }
+        let accessToken = gidsigninresult.user.accessToken.tokenString
+        
+        let tokens = GoogleSignInResultModel(idToken: idToken, accessToken: accessToken)
+        //try await AuthViewModel.shared.signinwithgoogle(String)
+        
+    }
+}
+
+
+struct LoginView: View {
+    @State private var Email = ""
+    @State private var Password = ""
+    @EnvironmentObject var viewmodel: AuthViewModel
+    @StateObject private var viewModel = AuthenticationViewModel()
+    //for apple login
+    @StateObject var logindata = AuthViewModel()
+    
+    var body: some View {
+        NavigationStack{
+            VStack{
+                //image
+                Image("Forge_logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height:120)
+                    .padding(.vertical, 32)
+                    .cornerRadius(10)
+                
+                //form contents
+                VStack(spacing: 30){
+                    InputView(text:$Email,
+                              title:"Email Address",
+                              placeholder:"name@example.com")
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                    InputView(text:$Password,
+                              title: "Password",
+                              placeholder: "enter password",
+                              issecurefield: true)
+                }
+                
+                .padding(.horizontal)
+                .padding(.top, 45)
+                
+                //signin button
+                Button{
+                    Task{
+                        try await viewmodel.signin(withemail: Email, password: Password)
+                    }
+                }
+            label: {
+                HStack{
+                    Text("Sign in")
+                        .fontWeight(.bold)
+                    Image(systemName: "arrow.right")
+                }
+                .foregroundColor(.white)
+                .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+            }
+            .background(Color(.systemBlue))
+            .cornerRadius(10)
+            .disabled(!formisvalid)
+            .opacity(formisvalid ? 1.0 : 0.5)
+            .padding(.top, 24)
+                Spacer()
+                
+                
+                
+//                SignInWithAppleButton(.signIn, onRequest: { request in
+//                                       request.requestedScopes = [.fullName, .email]
+//                                       logindata.nonce = logindata.randomNonceString()
+//                                       request.nonce = logindata.sha256(logindata.nonce)
+//                                   }, onCompletion: { result in
+//                                       logindata.handleAppleSignIn(result: result)
+//                                   })
+//                                   .signInWithAppleButtonStyle(.white)
+//                                   .frame(height: 55)
+//                                   .clipShape(Capsule())
+//                                   .padding(.horizontal, 100)
+//                                   .offset(y: 70)
+//                                   
+//                                   Spacer()
+//            
+                
+                
+                
+                    Spacer()
+                
+                //signup button
+                NavigationLink{
+                    RegistrationView()
+                        .environmentObject(viewmodel)
+                        .navigationBarBackButtonHidden(true)
+                    
+                }
+            label: {
+                HStack(spacing: 3){
+                    Text("Dont have an account?")
+                    Text("Sign Up!")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+                .font(.system(size: 15))
+            }
+                
+                
+                
+                
+                
+            }
+        }
+    }
+}
+    extension LoginView: AuthenticationFormProtocol{
+        var formisvalid: Bool{
+            return !Email.isEmpty
+            && Email.contains("@")
+            && !Password.isEmpty
+            && Password.count > 6
+        }
+    }
+
+
+struct LoginView_preview: PreviewProvider{
+    static var previews: some View{
+        LoginView()
+    }
+}
---
+++ InsightforgeX/Core/Authentication/View/MyLoggedInView.swift
@@ -0,0 +1,18 @@
+//
+//  MyLoggedInView.swift
+//  InsightforgeX
+//
+//  Created by Srivastava, Abhisht on 11/03/24.
+//
+
+import SwiftUI
+
+struct MyLoggedInView: View {
+    var body: some View {
+        Text("Logged In")
+    }
+}
+
+#Preview {
+    MyLoggedInView()
+}
