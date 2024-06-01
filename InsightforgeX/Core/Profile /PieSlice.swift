57b9563eaf53d9bbe3ff971e8ed43d257e03ff9d
Abhisht Srivastava <abhisht.srivastava@sap.com>
2024-06-01 15:55:03 +0530

    improving login view UI

---
+++ InsightforgeX/Core/Profile /PieSlice.swift
@@ -0,0 +1,26 @@
+//
+//  PieSlice.swift
+//  InsightforgeX
+//
+//  Created by Srivastava, Abhisht on 23/05/24.
+//
+
+//import SwiftUI
+//
+//struct PieSlice: Shape {
+//    var startAngle: Angle
+//    var endAngle: Angle
+//
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//        let center = CGPoint(x: rect.midX, y: rect.midY)
+//        let radius = min(rect.width, rect.height) / 2
+//
+//        path.move(to: center)
+//        path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+//        path.closeSubpath()
+//
+//        return path
+//    }
+//}
+//
